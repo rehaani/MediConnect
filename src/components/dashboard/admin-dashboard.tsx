d7f45a16ea8959879ef9468d5fdaca5ecfa04d2e
Rehaan Ahmad <rehaan1211ahmad@gmail.com>
2025-09-19 13:24:10 +0000

    Remove the Total Revenue card from Admin's Dashboard. And hide the docum

--- src/components/dashboard/admin-dashboard.tsx
+++ src/components/dashboard/admin-dashboard.tsx
@@ -9,16 +9,6 @@ const AdminDashboard = ({ user }: { user: User }) => {
       {/* Key Metrics */}
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-          <IndianRupee className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">₹4,52,318.89</div>
-          <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Consultations</CardTitle>
           <Stethoscope className="h-4 w-4 text-muted-foreground" />
         </CardHeader>
