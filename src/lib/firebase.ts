2dabd7c44f0ac85280b7b9679b47a2269113b515
Rehaan Ahmad <rehaan1211ahmad@gmail.com>
2025-09-20 12:56:07 +0000

    Fix it. Either by book or by crook

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,7 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
+const firebaseConfig = {"apiKey":"API_KEY","authDomain":"PROJECT_ID.firebaseapp.com","projectId":"PROJECT_ID","storageBucket":"PROJECT_ID.appspot.com","messagingSenderId":"SENDER_ID","appId":"APP_ID","measurementId":"G-MEASUREMENT_ID"};
 
 
 // Initialize Firebase
